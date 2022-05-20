4c12fef0fadc5994bc8279fdec3582ee011d85f2
leezzangmin <ckdals12345678@gmail.com>
2022-05-21 00:04:55 +0900

    item55 옵셔널 반환은 신중히 하라_2

--- src/item55/ReturnOptionalEx.java
+++ src/item55/ReturnOptionalEx.java
@@ -1,6 +1,7 @@
 package item55;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -32,8 +33,19 @@ public class ReturnOptionalEx {
         return Optional.of(result);
     }
 
+    public static <E extends Comparable<E>> Optional<E> max_improved_stream(Collection<E> c) {
+        return c.stream().max(Comparator.naturalOrder());
+    }
 
     public static void main(String[] args) {
+        //기본값 가능
+        // String lastWordInLexicon = max(word).orElse("단어 없음");
+
+        //원하는 예외 던지기 가능
+        // Toy myToy max(toys).orElseThrow(TemperTantrumException::new);
+
+        //항상 값이 채워져 있다고 가정할 때
+        // Element lastNobleGas = max(elements.NOBLE_GASES).get();
 
     }
 }
